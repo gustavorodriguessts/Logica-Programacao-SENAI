f51665e605ae0bfc48c2116fc59912d347797e5e
unknown <gustavordrigues848@gmail.com>
2025-02-17 16:22:58 -0300

    exercicio teste

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        double grausCelcius = 20;
+        double grausFahrenheit = (grausCelcius * 9/5) + 32;
+
+        System.out.println("a temperatura " + grausCelcius + "em fahrenheit é de " + grausFahrenheit);
+    }
+
+}
